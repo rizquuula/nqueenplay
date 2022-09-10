447d9d45caf6b82a212273e829683109f2539639
M Razif Rizqullah <razifrizqullah@gmail.com>
2022-09-10 12:49:12 +0700

    add setup

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 from nqueenplay import NQueen
 
-# N = 4
-# nqueens = NQueen(n=N, number_lock=1)
-# nqueens.get_attack_pairs()
-
-# for i in range(N):
-#     nqueens.move_random(i)
-    
-# nqueens.show()
-# nqueens.show_attacking_pairs()
-
-N = 8
+N = 4
 nqueens = NQueen(n=N, number_lock=1)
 nqueens.show()
 nqueens.show_attacking_pairs()
---
+++ setup.py
@@ -0,0 +1,37 @@
+import pathlib
+from setuptools import setup
+
+from nqueenplay.version import VERSION
+
+
+BASE_DIR = pathlib.Path(__file__).parent
+README = (BASE_DIR / "README.md").read_text()
+
+setup(
+    name="nquuenplay",
+    version=VERSION,
+    author="eiproject",
+    author_email="razifrizqullah@gmail.com",
+    url='https://github.com/eiproject/nqueenplay',
+    description='N-Queens puzzle player. This tool will generate randomly or randomly locked puzzle, you may use this as "a boxing bag" to practice problem solving algorithm.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=[
+        'nqueenplay', 
+        ],
+    include_package_data=True,
+    install_requires=[],
+    keywords=['n-queens', 'nqueens', 'puzzle'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
